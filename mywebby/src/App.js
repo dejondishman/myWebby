40c17d9caf9f38969b990281d8ab01b09febb37a
Dejon Dishman <dejondishman30@gmail.com>
2022-03-04 14:53:22 -0700

    updates

---
+++ mywebby/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Content from './Components/Content'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
---
+++ mywebby/src/Components/Content.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Content.css'
+
+
+const Content = () => {
+    return(
+        <div className="i">
+            <div className="i-left">left</div>
+            <div className="i-right">right</div>
+        </div>
+    )
+}
+
+
+export default Content
